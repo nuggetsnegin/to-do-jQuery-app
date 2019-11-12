d39528cb3439d90f53e28a5ec97e6e07d04703c5
Negin <nuggetnegin@gmail.com>
2019-11-12 10:55:12 -0500

    clean up indents

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// INSERT CODE HERE
-    $(document).ready(function(){ /*check if jquery is reji*/
-        console.log('ready');
+$(document).ready(function () {
+    /*check if jquery is reji*/
+    console.log('ready');
 
-        $('form').on('submit', function(e){ /*submit is the event, form is where the event happens*/
+    $('form').on('submit', function (e) {
+        /*submit is the event, form is where the event happens*/
         /*preventdefault  does not accept any arguments.*/
-            e.preventDefault();/*by default form refreshes page, remove default*/
-            console.log('submmited!');
-            
-    /*The .val() method is primarily used to get the values of form elements such as input, select and textarea. When called on an empty collection, it returns undefined.*/
-            const toDoItem = $('input').val(); /*This method does not accept any arguments.*/
-            /*val() is a getter and setter*/
-            console.log(toDoItem);
+        e.preventDefault(); /*by default form refreshes page, remove default*/
+        console.log('submmited!');
+
+        /*The .val() method is primarily used to get the values of form elements such as input, select and textarea. When called on an empty collection, it returns undefined.*/
+        const toDoItem = $('input').val(); /*This method does not accept any arguments.*/
+        /*val() is a getter and setter*/
+        console.log(toDoItem);
 
 
-            /*or can do if(toDoItem) the truthy value */
-            if(toDoItem !== ''){ /*if to do isnt empty*/
+        /*or can do if(toDoItem) the truthy value */
+        if (toDoItem !== '') {
+            /*if to do isnt empty*/
             /*This method is used to set a string of text, a number, an array of strings corresponding to the value of each matched element. This method facilitates you to set the value by passing in the function. */
-                $('input').val(''); /*set value to blank*/
+            $('input').val(''); /*set value to blank*/
             /*The .append() method inserts the specified content as the last child of each element in the jQuery collection (To insert it as the first child, use .prepend()).*/
-                $('ul').append(`<li><span class="fa fa-square-o"></span>${toDoItem}</li>`);
-            }
+            $('ul').append(`<li><span class="fa fa-square-o"></span>${toDoItem}</li>`);
+        }
 
-        }); 
     });
+});
